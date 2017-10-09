4fa2e473599f47ee9ef3c2edfb071ad5a971b0c6
DEEPTI SHARMA <31379739+DEEZZU@users.noreply.github.com>
2017-10-09 23:30:46 +0530

    Function declaration, definition, and call.

---
+++ FunctionsCallByValue.cpp
@@ -0,0 +1,33 @@
+//Functions in cpp
+#include<iostream>
+using namespace std;
+
+int retMax(int,int);
+
+int main(){
+
+int num1, num2;
+int res;
+
+cout << "\n Enter first value : " ;
+cin >> num1 ;
+cout << "\n Enter second value : " ;
+cin >> num2 ;
+
+res=retMax( num1, num2);
+
+cout << "\n The Maximum of two is : " << res ;
+
+return 0;
+}
+
+int retMax( int num1, int num2){
+
+if ( num1 > num2 ){
+  return num1;
+}
+else {
+  return num2;
+}
+
+}
