f965e6f5d73cdee7d518b1152adddd14e6b3b0af
DEEPTI SHARMA <31379739+DEEZZU@users.noreply.github.com>
2017-10-10 07:11:53 +0530

    Function using Call by Reference.

---
+++ FunctionCallByReference.cpp
@@ -0,0 +1,27 @@
+//Functions using call by reference 
+#include<iostream>
+using namespace std;
+
+void sum(int& , int, int);
+
+int main() {
+	int num1, num2;
+	int sumResult;
+	
+	cout << "\n Enter any two numbers : " ;
+	cout << "\n Enter num1 : " ;
+	cin >> num1 ;
+	cout << "\n Enter num2 : " ;
+	cin >> num2 ;
+	
+	sum(sumResult,num1,num2);
+	
+	cout << "\n Result of Addition : " << sumResult ;
+	
+	return 0;
+}
+
+void sum ( int& Res , int n1 , int n2 ){
+	
+	Res=n1+n2;
+}
