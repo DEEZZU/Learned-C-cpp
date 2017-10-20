108d641c97669e75febe77f93ce6fbbab323167c
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2017-10-20 19:38:26 +0530

    Random Number Generator

---
+++ RANDOM NUMBER/RANDOM NUMBER/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  RANDOM NUMBER
+//
+//  Created by DEEPTI SHARMA on 15/10/17.
+//  Copyright © 2017 DEEPTI SHARMA. All rights reserved.
+//
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int i, randVar;
+    time_t timeVar;
+    
+    timeVar = time(NULL) ;
+    srand ((unsigned) timeVar ) ; // this causes different random values on every executions
+    
+    for ( i=0 ; i<10 ; i++ ) {
+        randVar = rand();
+        cout << "\n Random Variable : " << randVar ;
+    }
+    
+    return 0;
+}
