d80ec59671b77ecfeba546ecf61be4aa39ee96ef
DEEPTI SHARMA <deepti.mcs17.du@gmail.com>
2017-11-03 10:33:59 +0530

    Created Hello World Program.

---
+++ LEARN C++/LEARN C++/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.cpp
+//  LEARN C++
+//
+//  Created by DEEPTI SHARMA on 03/11/17.
+//  Copyright © 2017 DEEPTI SHARMA. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "\n Hello World !!! " ;
+}
